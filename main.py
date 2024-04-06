18eabd5b8dfa2b64296fd20e2b066d51efb94d2c
030 <chocolatey030@gmail.com>
2024-04-06 11:45:45 +0200

    fix: [#22] Remove empty lines and comments

--- main.py
+++ main.py
@@ -13,11 +13,9 @@ GO_VERSIONS_URL = 'https://go.dev/dl/?mode=json'
 def determine_whether_go_version_in_go_mod_file_contains_patch_version() -> (str, bool):
     patch = False
     version = ""
-
     try:
         with open(GO_MOD_FILE, 'r') as file:
             major, minor, patch = "", "", ""
-
             content = file.read()
             go_version = re.search(r'go\s(\d+)\.(\d+)\.?(\d+)?', content)
             if go_version:
@@ -29,7 +27,6 @@ def determine_whether_go_version_in_go_mod_file_contains_patch_version() -> (str
                 logging.debug(f"patch found in go.mod: {patch}")
             else:
                 raise ValueError(f"no golang version defined in file: {GO_MOD_FILE}")
-
             if patch:
                 version = major+"."+minor+"."+patch
                 logging.debug(f"go version consists of major, minor and patch: {version}")
@@ -39,9 +36,7 @@ def determine_whether_go_version_in_go_mod_file_contains_patch_version() -> (str
                 logging.debug(f"go version consists of major and minor: {version}")
     except FileNotFoundError:
         raise ValueError(f"File not found: {GO_MOD_FILE}")
-
     logging.debug(f"current golang version that is defined in the go.mod: {version}")
-
     return version, patch
 
 
@@ -51,14 +46,11 @@ def get_latest_go_version():
     try:
         response = requests.get(GO_VERSIONS_URL)
         response.raise_for_status()
-
         data = response.json()
         latest_version = data[0]['version']
         logging.debug(f"latest go version: {latest_version} according to: {GO_VERSIONS_URL}")
-
         version_regex = re.compile(r'go(\d+)\.(\d+)\.(\d+)')
         match_data = version_regex.match(latest_version)
-
         if match_data:
             major, minor, patch = match_data.groups()
         else:
@@ -66,7 +58,6 @@ def get_latest_go_version():
     except requests.exceptions.RequestException as e:
         logging.info(f"Error fetching data: {GO_VERSIONS_URL} {e}")
         sys.exit(1)
-
     logging.debug(f"latest go major: {major}")
     logging.debug(f"latest go minor: {minor}")
     logging.debug(f"latest go patch: {patch}")
@@ -76,17 +67,11 @@ def get_latest_go_version():
 
 def regex_replace_go_version_in_go_mod_file(current_version: str, replacement: str):
     try:
-        # Read the content of the file
         with open(GO_MOD_FILE, 'r') as file:
             content = file.read()
-
-        # Use re.sub to replace the pattern with the replacement
         modified_content = re.sub(GO_MOD_GO_VERSION_REGEX, "go "+replacement, content)
-
-        # Write the modified content back to the file
         with open(GO_MOD_FILE, 'w') as file:
             file.write(modified_content)
-
         logging.info(
             f"bump golang version in go.mod file from {current_version} to {replacement}")
     except FileNotFoundError:
@@ -105,11 +90,8 @@ def configure_logging():
 
 def main():
     configure_logging()
-
     latest_major, latest_minor, latest_patch = get_latest_go_version()
-
     current_version, patch = determine_whether_go_version_in_go_mod_file_contains_patch_version()
-
     if patch:
         regex_replace_go_version_in_go_mod_file(current_version, latest_major+"."+latest_minor+"."+latest_patch)
     else:
