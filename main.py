f61c9ae1b6ca2ea276448e3c3abc0ee12b52f536
Ben van B <030@users.noreply.github.com>
2024-08-11 16:25:56 +0200

    fix: [#15] Let action update the PR title (#48)

--- main.py
+++ main.py
@@ -1,36 +1,50 @@
 import logging
-import os
 import re
 import requests
 import sys
+from typing import Tuple
 
 
 GO_MOD_FILE = "go.mod"
 GO_MOD_GO_VERSION_REGEX = r"go\s\d+.*"
-GO_VERSIONS_URL = 'https://go.dev/dl/?mode=json'
+GO_VERSIONS_URL = "https://go.dev/dl/?mode=json"
 
 
-def determine_whether_go_version_in_go_mod_file_contains_patch_version() -> (str, bool):
+def determine_whether_go_version_in_go_mod_file_contains_patch_version() -> (
+    Tuple[str, bool]
+):
     patch = False
     version = ""
     try:
-        with open(GO_MOD_FILE, 'r') as file:
+        with open(GO_MOD_FILE, "r") as file:
             major, minor, patch = "", "", ""
             content = file.read()
-            go_version = re.search(r'go\s(\d+)\.(\d+)\.?(\d+)?', content)
+            go_version = re.search(r"go\s(\d+)\.(\d+)\.?(\d+)?", content)
             if go_version is None:
-                raise ValueError(f"no golang version defined in file: {GO_MOD_FILE}")
-            major, minor, patch = go_version.group(1), go_version.group(2), go_version.group(3)
-            logging.debug(f"major: {major}, minor: {minor} and patch: {patch} found in go.mod")
+                raise ValueError(
+                    f"no golang version defined in file: {GO_MOD_FILE}"
+                )
+            major, minor, patch = (
+                go_version.group(1),
+                go_version.group(2),
+                go_version.group(3),
+            )
+            logging.debug(
+                f"major: {major}, minor: {minor} and patch: {patch} found in go.mod"
+            )
             version = f"{major}.{minor}"
             if patch:
                 version = f"{major}.{minor}.{patch}"
                 patch = True
             logging.debug(f"go version: {version}. Patch: {patch}")
+            logging.debug(
+                f"current golang version that is defined in the go.mod: {version}"
+            )
+            return version, patch
     except FileNotFoundError:
         raise ValueError(f"file not found: {GO_MOD_FILE}")
-    logging.debug(f"current golang version that is defined in the go.mod: {version}")
-    return version, patch
+    except Exception as e:
+        raise ValueError(f"an error occurred: {e}")
 
 
 def get_latest_go_version():
@@ -39,9 +53,11 @@ def get_latest_go_version():
         response = requests.get(GO_VERSIONS_URL)
         response.raise_for_status()
         data = response.json()
-        latest_version = data[0]['version']
-        logging.debug(f"latest go version: {latest_version} according to: {GO_VERSIONS_URL}")
-        version_regex = re.compile(r'go(\d+)\.(\d+)\.(\d+)')
+        latest_version = data[0]["version"]
+        logging.debug(
+            f"latest go version: {latest_version} according to: {GO_VERSIONS_URL}"
+        )
+        version_regex = re.compile(r"go(\d+)\.(\d+)\.(\d+)")
         match_data = version_regex.match(latest_version)
         if match_data:
             major, minor, patch = match_data.groups()
@@ -54,15 +70,20 @@ def get_latest_go_version():
     return major, minor, patch
 
 
-def regex_replace_go_version_in_go_mod_file(current_version: str, replacement: str):
+def regex_replace_go_version_in_go_mod_file(
+    current_version: str, replacement: str
+):
     try:
-        with open(GO_MOD_FILE, 'r') as file:
+        with open(GO_MOD_FILE, "r") as file:
             content = file.read()
-        modified_content = re.sub(GO_MOD_GO_VERSION_REGEX, f"go {replacement}", content)
-        with open(GO_MOD_FILE, 'w') as file:
+        modified_content = re.sub(
+            GO_MOD_GO_VERSION_REGEX, f"go {replacement}", content
+        )
+        with open(GO_MOD_FILE, "w") as file:
             file.write(modified_content)
         logging.info(
-            f"bump golang version in go.mod file from {current_version} to {replacement}")
+            f"bump golang version in go.mod file from {current_version} to {replacement}"
+        )
     except FileNotFoundError:
         logging.info(f"file not found: {GO_MOD_FILE}")
     except Exception as e:
@@ -73,17 +94,24 @@ def configure_logging():
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s",
-        datefmt='%Y-%m-%d %H:%M:%S'
+        datefmt="%Y-%m-%d %H:%M:%S",
     )
 
 
 def main():
     configure_logging()
     latest_major, latest_minor, latest_patch = get_latest_go_version()
-    current_version, patch = determine_whether_go_version_in_go_mod_file_contains_patch_version()
-    regex_replace_go_version_in_go_mod_file(current_version, f"{latest_major}.{latest_minor}")
+    current_version, patch = (
+        determine_whether_go_version_in_go_mod_file_contains_patch_version()
+    )
     if patch:
-        regex_replace_go_version_in_go_mod_file(current_version, f"{latest_major}.{latest_minor}.{latest_patch}")
+        regex_replace_go_version_in_go_mod_file(
+            current_version, f"{latest_major}.{latest_minor}.{latest_patch}"
+        )
+        return
+    regex_replace_go_version_in_go_mod_file(
+        current_version, f"{latest_major}.{latest_minor}"
+    )
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,17 +1,28 @@
-import logging
 import os
 import pytest
 import re
 import unittest
-from main import get_latest_go_version, main, GO_MOD_FILE
+from main import (
+    get_latest_go_version,
+    regex_replace_go_version_in_go_mod_file,
+    main,
+    GO_MOD_FILE,
+)
+import unittest
+from unittest.mock import mock_open, patch, MagicMock
+import requests
+
+GO_MOD_FILE = "go.mod"
+GO_MOD_GO_VERSION_REGEX = r"go\s\d+\.\d+"
+GO_VERSIONS_URL = "https://mocked-url.com"
 
 
 def get_golang_version_from_go_mod_file() -> str:
     try:
-        with open(GO_MOD_FILE, 'r') as file:
+        with open(GO_MOD_FILE, "r") as file:
             file_content = file.read()
             print(f"Content of the file:\n{file_content}")
-            pattern = r'\d+\.\d+\.?\d+?'
+            pattern = r"\d+\.\d+\.?\d+?"
             matches = re.findall(pattern, file_content)
             print("Extracted values:", matches[0])
             return matches[0]
@@ -24,7 +35,7 @@ def get_golang_version_from_go_mod_file() -> str:
 def setup_helper_create_go_mod_with_a_golang_version(version: str):
     with open(GO_MOD_FILE, "w") as file:
         file.write("module github.com/030/gomod-go-version-updater-action\n\n")
-        file.write("go "+version+"\n\n")
+        file.write("go " + version + "\n\n")
         file.write("require (\n")
         file.write("github.com/go-openapi/errors v0.21.1\n")
         file.write("github.com/aws/aws-sdk-go v1.50.30\n")
@@ -32,7 +43,6 @@ def setup_helper_create_go_mod_with_a_golang_version(version: str):
 
 
 def cleanup_helper():
-    exception_occurred = False
     try:
         if os.path.exists(GO_MOD_FILE):
             os.remove(GO_MOD_FILE)
@@ -50,18 +60,30 @@ class TestUpdateGolangVersionInGoModFile(unittest.TestCase):
     def test_update_golang_version_consisting_of_major_minor_and_patch(self):
         setup_helper_create_go_mod_with_a_golang_version("1.2.3")
         main()
-        self.assertEqual(get_golang_version_from_go_mod_file(), TestUpdateGolangVersionInGoModFile.latest_major+"." +
-                         TestUpdateGolangVersionInGoModFile.latest_minor+"."+TestUpdateGolangVersionInGoModFile.latest_patch)
+        self.assertEqual(
+            get_golang_version_from_go_mod_file(),
+            TestUpdateGolangVersionInGoModFile.latest_major
+            + "."
+            + TestUpdateGolangVersionInGoModFile.latest_minor
+            + "."
+            + TestUpdateGolangVersionInGoModFile.latest_patch,
+        )
 
     def test_update_golang_version_consisting_of_major_and_minor(self):
         setup_helper_create_go_mod_with_a_golang_version("4.2")
         main()
-        self.assertEqual(get_golang_version_from_go_mod_file(), TestUpdateGolangVersionInGoModFile.latest_major +
-                         "."+TestUpdateGolangVersionInGoModFile.latest_minor)
+        self.assertEqual(
+            get_golang_version_from_go_mod_file(),
+            TestUpdateGolangVersionInGoModFile.latest_major
+            + "."
+            + TestUpdateGolangVersionInGoModFile.latest_minor,
+        )
 
     def test_update_golang_version_consisting_of_major(self):
         setup_helper_create_go_mod_with_a_golang_version("42")
-        with pytest.raises(ValueError, match="no golang version defined in file: go.mod"):
+        with pytest.raises(
+            ValueError, match="no golang version defined in file: go.mod"
+        ):
             main()
 
     def test_update_golang_version_if_go_mod_does_not_exist(self):
@@ -69,5 +91,148 @@ class TestUpdateGolangVersionInGoModFile(unittest.TestCase):
             main()
 
 
-if __name__ == '__main__':
+class TestGetLatestGoVersion(unittest.TestCase):
+    @patch("requests.get")
+    def test_successful_fetch(self, mock_get):
+        mock_response = MagicMock()
+        mock_response.json.return_value = [{"version": "go1.18.3"}]
+        mock_response.status_code = 200
+        mock_get.return_value = mock_response
+
+        major, minor, patch = get_latest_go_version()
+
+        self.assertEqual(major, "1")
+        self.assertEqual(minor, "18")
+        self.assertEqual(patch, "3")
+
+    @patch("requests.get")
+    def test_non_matching_version_format(self, mock_get):
+        mock_response = MagicMock()
+        mock_response.json.return_value = [{"version": "invalid_version"}]
+        mock_response.status_code = 200
+        mock_get.return_value = mock_response
+
+        major, minor, patch = get_latest_go_version()
+
+        self.assertEqual(major, "")
+        self.assertEqual(minor, "")
+        self.assertEqual(patch, "")
+
+    @patch("requests.get")
+    def test_http_error(self, mock_get):
+        mock_get.side_effect = requests.exceptions.RequestException(
+            "Network error"
+        )
+
+        with self.assertRaises(SystemExit):  # Expecting the function to exit
+            get_latest_go_version()
+
+
+class TestRegexReplaceGoVersionInGoModFile(unittest.TestCase):
+    @patch(
+        "builtins.open",
+        new_callable=mock_open,
+        read_data="module example.com\n\ngo 1.18\n",
+    )
+    @patch("logging.info")
+    def test_successful_replacement(self, mock_logging_info, mock_file):
+        current_version = "1.18"
+        replacement = "1.19"
+
+        regex_replace_go_version_in_go_mod_file(current_version, replacement)
+
+        mock_file().write.assert_called_once_with(
+            "module example.com\n\ngo 1.19\n"
+        )
+        mock_logging_info.assert_called_once_with(
+            f"bump golang version in go.mod file from {current_version} to {replacement}"
+        )
+
+    @patch("builtins.open", side_effect=FileNotFoundError)
+    @patch("logging.info")
+    def test_file_not_found(self, mock_logging_info, mock_file):
+        current_version = "1.18"
+        replacement = "1.19"
+
+        regex_replace_go_version_in_go_mod_file(current_version, replacement)
+
+        mock_logging_info.assert_called_once_with(
+            f"file not found: {GO_MOD_FILE}"
+        )
+
+    @patch(
+        "builtins.open",
+        new_callable=mock_open,
+        read_data="module example.com\n\ngo 1.18\n",
+    )
+    @patch("logging.info")
+    def test_general_exception(self, mock_logging_info, mock_file):
+        # Simulate an exception other than FileNotFoundError
+        mock_file.side_effect = Exception("Some error")
+
+        current_version = "1.18"
+        replacement = "1.19"
+
+        regex_replace_go_version_in_go_mod_file(current_version, replacement)
+
+        mock_logging_info.assert_called_once_with(
+            "an error occurred: Some error"
+        )
+
+
+class TestMainFunction(unittest.TestCase):
+    @patch("main.regex_replace_go_version_in_go_mod_file")
+    @patch(
+        "main.determine_whether_go_version_in_go_mod_file_contains_patch_version"
+    )
+    @patch("main.get_latest_go_version")
+    @patch("main.configure_logging")
+    def test_main_with_patch_version(
+        self,
+        mock_configure_logging,
+        mock_get_latest_go_version,
+        mock_determine_version,
+        mock_regex_replace,
+    ):
+        # Mocking the return values of the dependencies
+        mock_get_latest_go_version.return_value = ("1", "19", "3")
+        mock_determine_version.return_value = ("1.18.0", True)
+
+        # Run the main function
+        main()
+
+        # Check that the functions were called with the correct arguments
+        mock_configure_logging.assert_called_once()
+        mock_get_latest_go_version.assert_called_once()
+        mock_determine_version.assert_called_once()
+        mock_regex_replace.assert_called_once_with("1.18.0", "1.19.3")
+
+    @patch("main.regex_replace_go_version_in_go_mod_file")
+    @patch(
+        "main.determine_whether_go_version_in_go_mod_file_contains_patch_version"
+    )
+    @patch("main.get_latest_go_version")
+    @patch("main.configure_logging")
+    def test_main_without_patch_version(
+        self,
+        mock_configure_logging,
+        mock_get_latest_go_version,
+        mock_determine_version,
+        mock_regex_replace,
+    ):
+        # Mocking the return values of the dependencies
+        mock_get_latest_go_version.return_value = ("1", "19", "3")
+        mock_determine_version.return_value = ("1.18", False)
+
+        # Run the main function
+        main()
+
+        # Check that the functions were called with the correct arguments
+        mock_configure_logging.assert_called_once()
+        mock_get_latest_go_version.assert_called_once()
+        mock_determine_version.assert_called_once()
+        mock_regex_replace.assert_called_once_with("1.18", "1.19")
+
+
+if __name__ == "__main__":
     unittest.main()
